88ba88b7ce05116d054b7bda025435d6c9494b76
unknown <t@k327-200.eszi.local>
2026-01-29 08:46:51 +0100

    initial

---
+++ eszi.demo.client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import plugin from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [plugin()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
